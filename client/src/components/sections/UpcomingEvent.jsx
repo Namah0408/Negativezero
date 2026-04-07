5742e0fc4f584058be887e83286fb91d7e5ec17e
Namah Agrawal <namahagrawal85@gmail.com>
2026-04-07 12:19:28 +0530

    commit 6

--- client/src/components/sections/UpcomingEvent.jsx
+++ client/src/components/sections/UpcomingEvent.jsx
@@ -8,14 +8,14 @@ export default function UpcomingEvent() {
         <SectionHeading title="Next Edition" />
         
         <div className="text-center space-y-6">
-          <p className="text-3xl font-bold">12th April 2026</p>
+          <p className="text-3xl font-bold">2nd & 3rd May 2026</p>
           <p className="text-gray-400">
             SCZCC Nagpur | 4000+ Expected Visitors
           </p>
 
           {/* Brochure Button */}
           <a
-            href="/brochure/NegativeZeroBrochure!.pdf.pdf"
+            href="/brochure/NZ2026brochure.pdf.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block mt-4 px-8 py-3 bg-white text-black font-semibold rounded-full hover:scale-105 transition"
